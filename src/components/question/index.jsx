64157ce237fb8c132f2df71ae7672f7f81d5e72f
Pablo <pablo.kalyell.441@gmail.com>
2022-11-28 16:51:31 -0200

    add button restart

--- src/components/question/index.jsx
+++ src/components/question/index.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { QuizContext } from "../../context/quiz";
 import Option from "../option";
 import { AiOutlineArrowRight } from "react-icons/ai";
+import { VscDebugRestart } from "react-icons/vsc";
 import "./styles.css";
 
 function Question() {
@@ -20,6 +21,10 @@ function Question() {
       <p>
         Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length}
       </p>
+      <button  className="btnreiniciar" onClick={() => dispatch({ type: "NEW_GAME" })}>
+        <VscDebugRestart style={{ position: "relative", top: 3, right: 4 }} />
+        Reiniciar{" "}
+      </button>
       <h2>{currentQuestion.question}</h2>
       <div id="options-container">
         <p>
@@ -39,6 +44,7 @@ function Question() {
           <AiOutlineArrowRight
             style={{ position: "relative", top: 3, left: 3 }}
           />
+           
         </button>
       )}
     </div>
--- src/data/questions.js
+++ src/data/questions.js
@@ -12,105 +12,194 @@ const data = [
 
   {
     question: "Qual regra altera a cor de um elemento?",
-    options: ["color", "background-color", "font-size", "transition"],
+    options: [
+    "color",
+    "background-color",
+    "font-size",
+    "transition"
+  ],
     answer: "color",
   },
   {
     question: "Para aumentar a fonte de um elemento utilizamos:",
-    options: ["font", "text-transform", "font-size", "hover"],
+    options: [
+    "font",
+    "text-transform",
+    "font-size",
+    "hover"
+  ],
     answer: "font-size",
   },
   {
     question: "A posição que deixa um elemento fixo é a:",
-    options: ["static", "absolute", "fixed", "relative"],
+    options: [
+    "static",
+    "absolute",
+    "fixed",
+    "relative"
+  ],
     answer: "fixed",
   },
   {
     question: "Qual tag cria um parágrafo?",
-    options: ["<p>", "<h1>", "<text>", "<ul>"],
+    options: [
+    "<p>",
+    "<h1>",
+    "<text>",
+    "<ul>"
+  ],
     answer: "<p>",
   },
   {
     question: "Qual atributo adiciona um link para a tag a?",
-    options: ["alt", "href", "src", "link"],
+    options: [
+    "alt",
+    "href",
+    "src",
+    "link"
+  ],
     answer: "href",
   },
   {
     question: "As listas não ordenadas tem a tag de:",
-    options: ["<ol>", "<ul>", "<li>", "<list>"],
+    options: [
+    "<ol>",
+    "<ul>",
+    "<li>",
+    "<list>"
+  ],
     answer: "<ul>",
   },
   {
     question: "Qual atributo deixa o input obrigatório?",
-    options: ["placeholder", "value", "required", "maxlength"],
+    options: [
+    "placeholder",
+    "value",
+    "required",
+    "maxlength"
+  ],
     answer: "required",
   },
   {
     question: "A tag semântica indicada para rodapés é a:",
-    options: ["div", "main", "section", "footer"],
+    options: [
+    "div",
+    "main",
+    "section",
+    "footer"
+  ],
     answer: "footer",
   },
   {
     question: "Com qual instrução declaramos uma constante em JavaScript?",
-    options: ["const", "let", "var", "define"],
+    options: [
+    "const",
+    "let",
+    "var",
+    "define"
+  ],
     answer: "const",
   },
   {
     question: "Qual dos tipos de dado a seguir não existe em JavaScript?",
-    options: ["string", "number", "boolean", "float"],
+    options: [
+    "string",
+    "number",
+    "boolean",
+    "float"
+  ],
     answer: "float",
   },
   {
     question: "Qual dos métodos a seguir seleciona um elemento?",
-    options: ["querySelector", "parseInt", "sort", "reduce"],
+    options: [
+    "querySelector",
+    "parseInt",
+    "sort",
+    "reduce"
+  ],
     answer: "querySelector",
   },
   {
     question:
       "Qual destas propriedades da a quantidade de elementos de um array?",
-    options: ["qty", "length", "items", "index"],
+    options: [
+    "qty",
+    "length",
+    "items",
+    "index"
+  ],
     answer: "length",
   },
   {
-    question:
-      "O que significa HTML?",
-    options: ["HyperLinks and Text markup Language", "Hyper Text Markup Language", "Home Tool Markup Language"],
+    question: "O que significa HTML?",
+    options: [
+      "HyperLinks and Text markup Language",
+      "Hyper Text Markup Language",
+      "Home Tool Markup Language",
+    ],
     answer: "Hyper Text Markup Language",
   },
   {
     question:
       "Qual atributo HTML especifica um texto alternativo para uma imagem, se a imagem não puder ser exibida?",
-    options: ["longdesc", "title", "src", "alt"],
+    options: [
+    "longdesc",
+    "title",
+    "src",
+    "alt"
+  ],
     answer: "alt",
   },
   {
-    question:
-      "O que significa CSS?",
-    options: ["Computer Style Seets", "Cascading Style Sheets", "Creative Style Sheets", "Colorful Style Sheets"],
+    question: "O que significa CSS?",
+    options: [
+      "Computer Style Seets",
+      "Cascading Style Sheets",
+      "Creative Style Sheets",
+      "Colorful Style Sheets",
+    ],
     answer: "Cascading Style Sheets",
   },
   {
-    question:
-      "Como você insere um comentário em um arquivo CSS?",
-    options: ["//comentario//", "'comentario", "/*comentario*/", "//comentario"],
-    answer: "/*comentario*/"
+    question: "Como você insere um comentário em um arquivo CSS?",
+    options: [
+      "//comentario//",
+      "'comentario",
+      "/*comentario*/",
+      "//comentario",
+    ],
+    answer: "/*comentario*/",
   },
   {
     question:
       "Como você seleciona todos os elementos p dentro de um elemento div?",
-    options: ["div.p", "div p", "div + p ",],
+    options: [
+    "div.p",
+    "div p",
+    "div + p "
+  ],
     answer: "div p",
   },
   {
     question:
       "Como você adiciona uma cor de fundo para todos os elementos <h1>",
-    options: ["h1 {background-color:#FFF}", "all.h1 {background-color:#FFF}", "h1.all {background-color:#FFF}"],
+    options: [
+      "h1 {background-color:#FFF}",
+      "all.h1 {background-color:#FFF}",
+      "h1.all {background-color:#FFF}",
+    ],
     answer: "h1 {background-color:#FFF}",
   },
   {
     question:
       "Como você exibe uma borda como esta: A borda superior = 10 pixels, A borda inferior = 5 pixels, A borda esquerda = 20 pixels, A borda direita = 1 pixel?",
-    options: ["border-width:10px 20px 5px 1px", "border-width:10px 5px 20px 1px", "border-width: 5px 20px 10px 1px","border-width: 10px 1px 5px 20px"],
+    options: [
+      "border-width:10px 20px 5px 1px",
+      "border-width:10px 5px 20px 1px",
+      "border-width: 5px 20px 10px 1px",
+      "border-width: 10px 1px 5px 20px",
+    ],
     answer: "border-width:10px 20px 5px 1px",
   },
 ];
